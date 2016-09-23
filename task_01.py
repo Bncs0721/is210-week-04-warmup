c739df59fc7a7682a95465b20ad1fb60df887c39
Biencis Rubiera <biencis@gmail.com>
2016-09-23 20:21:59 +0000

    Created a docstring for module task_01.py

--- task_01.py
+++ task_01.py
@@ -4,6 +4,23 @@
 
 
 def know_what_i_mean(wink, numwink=2):
+    """Combines several variables into a string.
+
+    Args:
+        wink (str): Str to be repeated numwink times.
+        numwink (int, optional): Int by which wink will be repeated. Defautl: 2.
+
+    Returns:
+        str: All arguments concatenated as specified.
+
+    Examples:
+
+        >>> know_what_i_mean('zaul')
+        'Know what I mean? zaulzaul, nudge nudge'
+
+        >>> know_what_i_mean('5')
+        'Know what I mean? 55, nudge nudge'
+    """
     winks = (wink * numwink).strip()
     nudges = ('nudge ' * numwink).strip()
     retstr = 'Know what I mean? {}, {}'.format(winks, nudges)
