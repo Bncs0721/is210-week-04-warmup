f1acff99eb42ba4ee6ceca323f3964f668c2bcc9
Biencis Rubiera <biencis@gmail.com>
2016-09-24 06:49:15 +0000

    Created task_04 which containes function too_many_kittens

---
+++ task_04.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module tests too_many_kittens function."""
+
+def too_many_kittens(kittens, litterboxes, catfood=None):
+    """This function determines if there are too many kittens.
+
+    Args:
+        kittens(int):Number of kittens.
+        litterboxes(int):Number of litterboxes.
+        catfood(bool):Specifies whether there is catfood True(1) or not False(0).
+
+    Returns:
+        Bool: Answers whether or not we have too many kittens (True or False.
+
+    Examples:
+
+        >>> too_many_kittens(12, 12, False)
+        True
+
+        >>> too_many_kittens(13, 12, 1)
+        True
+
+    """
+    ANSWER = (not (litterboxes >= kittens and catfood))
+    return ANSWER
